ba1fd34580291593390e91fc4833410fd899dc8d
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2025-07-18 01:01:55 -0300

    Test quote API

--- app/quote_handler.py
+++ app/quote_handler.py
@@ -5,5 +5,5 @@ from service.quote_service import search_dollar_quote
 def lambda_handler(event, context):
     asyncio.run(search_dollar_quote())
 
-#if __name__ == "__main__":
- #  lambda_handler(None,None)    
+if __name__ == "__main__":
+  lambda_handler(None,None)
